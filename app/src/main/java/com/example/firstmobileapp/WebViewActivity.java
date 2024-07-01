96c65203f9b55beca1e7126afefd9ccb96f2d36f
mohamadjavad.saeidi <mohamadjavad.saeidi@snapp.cab>
2024-07-02 03:08:54 +0330

    add web view...

---
+++ app/src/main/java/com/example/firstmobileapp/WebViewActivity.java
@@ -0,0 +1,28 @@
+package com.example.firstmobileapp;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WebViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_webview);
+
+        WebView webView = findViewById(R.id.webview);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                view.loadData("Page not available", "text/html", "utf-8");
+            }
+        });
+        webView.loadUrl("https://mock-app.github.io/");
+    }
+}
