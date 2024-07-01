ce83f797caef3e8d70db814e2aaa81fa7f21f21a
mohamadjavad.saeidi <mohamadjavad.saeidi@snapp.cab>
2024-07-02 03:08:39 +0330

    add splash screen...

---
+++ app/src/main/java/com/example/firstmobileapp/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.example.firstmobileapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish(); // Finish SplashActivity to prevent it from being accessible when the back button is pressed
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
